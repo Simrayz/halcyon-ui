0bdf5ae2c8da44d0051e0981b8bdba654d303666
= <simen@sentinel.no>
2023-06-14 21:59:53 +0200

    🚸 Add exact property to NavLink

--- apps/halcyon/components/AppSidebar.tsx
+++ apps/halcyon/components/AppSidebar.tsx
@@ -7,23 +7,26 @@ export function AppSidebar() {
   return (
     <aside className="w-36 border-r border-gray-400 pt-2">
       <nav className="flex flex-col">
-        <NavLink href="/">Home</NavLink>
+        <NavLink href="/" exact>
+          Home
+        </NavLink>
         <NavLink href="/about">About</NavLink>
       </nav>
     </aside>
   );
 }
 
-type NavLinkProps = PropsWithChildren<{ href: string }>;
-function NavLink({ children, href }: NavLinkProps) {
+type NavLinkProps = PropsWithChildren<{ href: string; exact?: boolean }>;
+function NavLink({ children, href, exact }: NavLinkProps) {
   const pathname = usePathname();
+  const activeLink = exact ? pathname === href : pathname.startsWith(href);
 
   return (
     <Link
       href={href}
       className={[
         'px-4 py-2 w-full',
-        pathname === href
+        activeLink
           ? 'text-blue-600 hover:text-blue-600'
           : 'hover:text-blue-700',
       ].join(' ')}
